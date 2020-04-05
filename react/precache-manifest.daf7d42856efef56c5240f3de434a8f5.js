cfdad993c888123ee9b1339cac808949c82833d5
akp2020 <akp2020@yahoo.com>
2020-04-05 15:44:20 +0530

    added react default app

---
+++ react/precache-manifest.daf7d42856efef56c5240f3de434a8f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc44c7bccb0dba84008eeb6d3314f180",
+    "url": "/index.html"
+  },
+  {
+    "revision": "681e2f5a5f3dbf0c9bea",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c04d86f51124a704c2fb",
+    "url": "/static/js/2.e3928b1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e3928b1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "681e2f5a5f3dbf0c9bea",
+    "url": "/static/js/main.2c9053d4.chunk.js"
+  },
+  {
+    "revision": "aaca772fbf2317e112bb",
+    "url": "/static/js/runtime-main.76517ff2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

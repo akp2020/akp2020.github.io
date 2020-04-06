3c0083311c627c10571bfd66b3ab2f98854c6d9b
akp2020 <akp2020@yahoo.com>
2020-04-06 07:09:21 +0530

    react-src added

---
+++ react-src/README.md
@@ -0,0 +1,5 @@
+# My React discovery ..
+
+# This app will cover almost all important features of reactjs.
+
+# Stay tuned !!!
---
+++ react-src/moveBuild.js
@@ -0,0 +1,13 @@
+const fs = require("fs-extra");
+const rimraf = require("rimraf");
+/* Moving build to akp2020.github.io/react*/
+
+const source = "build";
+const destination = "../react";
+
+rimraf("../react/*", () => {
+  console.log("deleted");
+  fs.copy(source, destination, () => {
+    console.log("copied");
+  });
+});
